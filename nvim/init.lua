46e1d207810c9c676f0e66334bf663e6d48b1ad6
Lorenz Schmidt <git@lorenzschmidt.com>
2024-08-20 15:33:45 +0200

    Add gruvbox syntax highlighting

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,7 @@ require("config.lazy")
  -- set default tab width to four whitespaces
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+
+-- set default theme to light Gruvbox
+vim.o.background = "light" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
