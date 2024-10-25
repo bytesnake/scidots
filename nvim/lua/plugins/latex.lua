e3288e78c0a88d322f531c116382d7c4e4019390
Lorenz Schmidt <git@lorenzschmidt.com>
2024-10-25 13:47:22 +0200

    Add missing horizontal qualifier to window splitting for tmux

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -5,5 +5,6 @@ return {
     vim.g.vimtex_view_method = "general"
 	vim.g.vimtex_view_general_method = "okular"
 	vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+	vim.g.vimtex_fold_enabled = 1
   end
 }
